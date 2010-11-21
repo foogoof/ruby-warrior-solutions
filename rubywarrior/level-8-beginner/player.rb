f9811cf8e45dd757da46bb78b6af778ec5200bfa
seth-schroeder <foogoof+github@gmail.com>
2010-11-21 12:25:31 -0500

    solved level 8

--- rubywarrior/level-8-beginner/player.rb
+++ rubywarrior/level-8-beginner/player.rb
@@ -1,9 +1,33 @@
+require 'pp'
+
 class Player
   @last_known_health = nil
   RUNAWAY=(20 * 0.40).to_i
   BANZAI=(20 * 0.75).to_i
+
+  VISIBLE_THINGS=%w{ stairs empty wall captive enemy }
   
-  # screw the captive! muahahah!
+  def surveil(warrior)
+    line_of_sight = {}
+    warrior.look.each_with_index { |space, index|
+      index.succ
+      thing = VISIBLE_THINGS.select { |type|
+        space.send "#{type}?".to_sym
+      }.first
+      raise "woah" unless thing
+      line_of_sight[:nearest] = thing unless line_of_sight[:nearest]
+      if line_of_sight[thing]
+        line_of_sight[thing] << index
+      else
+        line_of_sight[thing] = [index]
+      end
+    }
+
+    pp line_of_sight
+    
+    line_of_sight
+  end
+  
   def play_turn(warrior)
     @last_known_health = warrior.health unless @last_known_health
     took_damage = @last_known_health > warrior.health
@@ -11,7 +35,18 @@ class Player
     if warrior.feel.wall?
       warrior.pivot!
     elsif warrior.feel.empty?
-      if took_damage
+      i_spy = surveil warrior
+      next_good_guy = i_spy.fetch("captive", []).first
+      next_bad_guy = i_spy.fetch("enemy", []).first
+      if next_good_guy && next_bad_guy
+        if next_good_guy < next_bad_guy
+          warrior.walk!
+        else
+          warrior.shoot!
+        end
+      elsif next_bad_guy
+        warrior.shoot!
+      elsif took_damage
         if warrior.health <= RUNAWAY
           warrior.walk! :backward
         else
