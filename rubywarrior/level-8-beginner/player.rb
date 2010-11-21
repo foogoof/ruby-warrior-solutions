c89e461e9df1c03a25e4c2c104887aef8f200dd8
seth-schroeder <foogoof+github@gmail.com>
2010-11-21 11:43:07 -0500

    before level 8

---
+++ rubywarrior/level-8-beginner/player.rb
@@ -0,0 +1,37 @@
+class Player
+  @last_known_health = nil
+  RUNAWAY=(20 * 0.40).to_i
+  BANZAI=(20 * 0.75).to_i
+  
+  # screw the captive! muahahah!
+  def play_turn(warrior)
+    @last_known_health = warrior.health unless @last_known_health
+    took_damage = @last_known_health > warrior.health
+
+    if warrior.feel.wall?
+      warrior.pivot!
+    elsif warrior.feel.empty?
+      if took_damage
+        if warrior.health <= RUNAWAY
+          warrior.walk! :backward
+        else
+          warrior.walk! :forward
+        end
+      else
+        if warrior.health < BANZAI
+          warrior.rest!
+        else
+          warrior.walk! :forward
+        end
+      end
+    else
+      if warrior.feel.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end
+    end
+
+    @last_known_health = warrior.health
+  end
+end
