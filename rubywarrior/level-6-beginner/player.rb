bf118fa778bd5a3d2651772ab3ffd168112d3ece
seth-schroeder <foogoof+github@gmail.com>
2010-11-21 11:14:31 -0500

    before level 6

---
+++ rubywarrior/level-6-beginner/player.rb
@@ -0,0 +1,25 @@
+class Player
+  @last_known_health = nil
+
+  def play_turn(warrior)
+    @last_known_health = warrior.health unless @last_known_health
+
+    if warrior.feel.empty?
+      if warrior.health < @last_known_health
+        warrior.walk!
+      elsif warrior.health < 13
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    else
+      if warrior.feel.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end
+    end
+
+    @last_known_health = warrior.health
+  end
+end
